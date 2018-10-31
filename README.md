d3b4886c147ba12f64d8f71c3183854db5c91cd3
Ian Turgeon <iturgeon@gmail.com>
2018-10-30 20:49:48 -0400

    update mwdk, readme

---
+++ README.md
@@ -0,0 +1,22 @@
+<h1>
+    <img src="src/_icons/icon-60.png" width="36px"/>
+    Last Chance Cadet: A Materia Widget
+</h1>
+
+Last Chance Cadet is a stylized matching game that provides a list of items that students are asked to match with corresponding items. Last Chance Cadet is designed for use with [Materia](https://github.com/ucfopen/Materia), an open-source platform for interactive course content developed by the University of Central Florida.
+
+![Last Chance Cadet Player](src/_screen-shots/1.png)
+
+### Basic Use
+
+In a production evironment, Last Chance Cadet is installed to your Materia instance and is accessed via the Widget Catalog. For development, Last Chance Cadet is bundled with the [Materia Widget Development Kit](https://github.com/ucfopen/Materia-Widget-Dev-Kit), which allows for rapid development in a local context using express.js.
+
+### Local Development
+
+Last Chance Cadet uses the Materia Widget Development Kit for location development. Check out [the docs for the MDK](https://ucfopen.github.io/Materia-Docs/develop/materia-widget-development-kit.html) to learn more about starting up the local express environment.
+
+### Installation to Materia
+
+Widget installation options are covered in the [Installing Widgets](https://ucfopen.github.io/Materia-Docs/admin/installing-widgets.html) section of the Materia Docs.
+
+For more information about the widget development process, be sure to visit the [widget developer guide](https://ucfopen.github.io/Materia-Docs/develop/widget-developer-guide.html) for Materia.
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,28 @@
 const path = require('path')
+const srcPath = path.join(__dirname, 'src') + path.sep
+const widgetWebpack = require('materia-widget-development-kit/webpack-widget')
 
-let matchingConfig = {}
-//override the demo with a copy that assigns ids to each question for dev purposes
-if (process.env.npm_lifecycle_script == 'webpack-dev-server') {
-	matchingConfig.demoPath = 'devmateria_demo.json'
-}
+const entries = widgetWebpack.getDefaultEntries()
 
-// load the reusable legacy webpack config from materia-widget-dev
-let webpackConfig = require('materia-widget-development-kit/webpack-widget').getLegacyWidgetBuildConfig(matchingConfig)
+delete entries['creator.js']
+delete entries['player.js']
 
-delete webpackConfig.entry['creator.js']
-delete webpackConfig.entry['player.js']
-
-webpackConfig.entry['modules/matching.js'] = [path.join(__dirname, 'src', 'modules', 'matching.coffee')]
-
-webpackConfig.entry['controllers/creator.js'] = [path.join(__dirname, 'src', 'controllers', 'creator.coffee')]
-webpackConfig.entry['controllers/player.js'] = [path.join(__dirname, 'src', 'controllers', 'player.coffee')]
-
-webpackConfig.entry['directives/audioControls.js'] = [path.join(__dirname, 'src', 'directives', 'audioControls.coffee')]
-webpackConfig.entry['directives/focusMe.js'] = [path.join(__dirname, 'src', 'directives', 'focusMe.coffee')]
-webpackConfig.entry['directives/ngEnter.js'] = [path.join(__dirname, 'src', 'directives', 'ngEnter.coffee')]
-webpackConfig.entry['directives/inputStateManager.js'] = [path.join(__dirname, 'src', 'directives', 'inputStateManager.coffee')]
-
-webpackConfig.entry['audioControls.css'] = [
-	path.join(__dirname, 'src', 'audioControls.scss'),
-	path.join(__dirname, 'src', 'audioControls.html')
+entries['modules/matching.js'] = [path.join(srcPath, 'modules', 'matching.coffee')]
+entries['controllers/creator.js'] = [path.join(srcPath, 'controllers', 'creator.coffee')]
+entries['controllers/player.js'] = [path.join(srcPath, 'controllers', 'player.coffee')]
+entries['directives/audioControls.js'] = [path.join(srcPath, 'directives', 'audioControls.coffee')]
+entries['directives/focusMe.js'] = [path.join(srcPath, 'directives', 'focusMe.coffee')]
+entries['directives/ngEnter.js'] = [path.join(srcPath, 'directives', 'ngEnter.coffee')]
+entries['directives/inputStateManager.js'] = [path.join(srcPath, 'directives', 'inputStateManager.coffee')]
+entries['audioControls.css'] = [
+	srcPath + 'audioControls.scss',
+	srcPath + 'audioControls.html'
 ]
 
-module.exports = webpackConfig
+// options for the build
+let options = {
+	entries: entries
+}
+
+module.exports = widgetWebpack.getLegacyWidgetBuildConfig(options)
+
